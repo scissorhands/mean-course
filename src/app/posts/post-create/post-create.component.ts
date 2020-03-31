b947a34cc030e2325e70aa3b8878419bf7b5bcfa
scissorhands <edward_beats>
2020-03-30 20:07:12 -0600

    header component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class PostCreateComponent {
   enteredValue = ''
   newPost = '';
-  onAddPost(postInput: HTMLTextAreaElement){
+  onAddPost(){
     this.newPost = this.enteredValue;
   }
 }
